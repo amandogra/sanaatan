4f0d78179f559c40ef61dabb1197be40287e4416
Aman Dogra <a_dogra@apple.com>
2022-02-09 20:39:56 -0800

    first draft of toc.md

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './styles/variables.css';
+import './styles/home.css';
+
+// const app = document.querySelector<HTMLDivElement>('#app')!;
+
+// app.innerHTML = `
+  // <h1>Hello!</h1>
+  // <p> Everything is in console </p>
+  // <p>Try manipulating <pre><code>src/main.ts</code></pre> </p>
+// `;
