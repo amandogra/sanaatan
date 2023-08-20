054db1747c729d720b4cefe1b95abf1ec4488b3f
Aman Dogra <amandogra@gmail.com>
2023-08-20 12:38:26 -0700

    Basic express JS server
    Using bun installed the express JS related dependencies. Following the tutoriat at: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/development_environment
    
    Just a basic express server. No bells and wistles

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import figlet from "figlet";
+import path from "path";
 
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  const body = figlet.textSync('Bun!');
+  const body = figlet.textSync("Bun!");
   res.send(body);
+});
+
+app.get("/test", (req, res) => {
+  res.sendFile(path.join(__dirname, "experiments", "index.html"));
+});
+
+app.get("/book", (req, res) => {
+  res.sendFile(path.join(__dirname, "experiments", "book.html"));
 });
 
 app.listen(port, () => {
